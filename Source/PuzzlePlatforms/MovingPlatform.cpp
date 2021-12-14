aea2fa81087cbe157a3f82e1e891ff63c171c0b2
BigRubberDucky <mclafferty.kursten1997@gmail.com>
2021-12-14 14:59:21 -0500

    End of lesson 8

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+	speed = 20.f;
+
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		SetReplicates(true);
+		SetReplicateMovement(true);
+	}
+
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if(HasAuthority())
+	{ 
+		FVector location = GetActorLocation();
+		location += FVector(speed * DeltaTime, 0.f, 0.f);
+		SetActorLocation(location);
+	}
+	
+
+}
